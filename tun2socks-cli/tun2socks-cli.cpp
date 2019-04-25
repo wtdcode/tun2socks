840590cd807719a90808f9d5603e0d24fb2448ea
Mio <a771644612@live.com>
2019-04-25 23:27:36 +0800

    Minor changes.

--- tun2socks-cli/tun2socks-cli.cpp
+++ tun2socks-cli/tun2socks-cli.cpp
@@ -1,4 +1,3 @@
-#include <Windows.h>
 #include "tun2socks.h"
 
 static const char* tap_ip = "10.2.3.1";
@@ -7,6 +6,7 @@ static const char* tap_mask = "255.255.255.252";
 
 static const char* socks5_address = "127.0.0.1";
 static const uint16_t socks5_port = 1080;
+static const uint32_t udp_timeout = 60000;
 
 int main()
 {
@@ -15,7 +15,13 @@ int main()
 	adapter->mask = inet_addr(tap_mask);
 	adapter->network = inet_addr(tap_network);
 	SOCKS5NoAuth auth{ NO_AUTH};
-	auto config = make_config_with_socks5_no_auth(adapter, socks5_address, strlen(socks5_address), 1080, 60000, &auth);
+	auto config = make_config_with_socks5_no_auth(
+		adapter, 
+		socks5_address, 
+		strlen(socks5_address), 
+		socks5_port, 
+		udp_timeout, 
+	&auth);
 	tun2socks_start(config);
 	delete_tun(adapter);
 	delete_config(config);
