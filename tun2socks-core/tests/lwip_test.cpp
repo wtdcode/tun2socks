604b4c39be825f4ba51332ab011e69a1af72485b
Mio <a771644612@live.com>
2019-05-23 14:31:34 +0800

    Add two tests about lwip

--- tun2socks-core/tests/lwip_test.cpp
+++ tun2socks-core/tests/lwip_test.cpp
@@ -5,14 +5,37 @@
 #include <lwip/init.h>
 #include <lwip/udp.h>
 #include <lwip/sys.h>
+#include <lwip/pbuf.h>
+
+void init(){
+    static bool initialized = false;
+    if(!initialized){
+        lwip_init();
+        initialized = true;
+    }
+}
 
 TEST_CASE("Test basic lwip functions"){
-    lwip_init();
+    init();
     SECTION("Test max udp_pcb"){
-        udp_pcb* arr[4096];
+        udp_pcb* u_arr[4096];
         for(int i = 0; i < 4096; i++){
-            arr[i] = udp_new();
-            REQUIRE(arr[i] != NULL);
+            u_arr[i] = udp_new();
+            REQUIRE(u_arr[i] != NULL);
+        }
+    }
+    SECTION("Test max tcp_pcb"){
+        tcp_pcb* t_arr[4096];
+        for(int i = 0;i < 4096; i++){
+            t_arr[i] = tcp_new();
+            REQUIRE(t_arr[i] != NULL);
+        }
+    }
+    SECTION("Test max pbuf"){
+        pbuf* pbuf_arr[4096];
+        for(int i = 0 ;i < 4096; i++){
+            pbuf_arr[i] = pbuf_alloc(pbuf_layer::PBUF_TRANSPORT, 1600, pbuf_type::PBUF_RAM);
+            REQUIRE(pbuf_arr[i] != NULL);
         }
     }
 }
