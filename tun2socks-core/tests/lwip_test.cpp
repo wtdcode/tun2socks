9fc51e438119d16cbf3bc4e7054a43dad1814e36
Mio <a771644612@live.com>
2019-05-23 15:22:48 +0800

    Add tests for sys_arch

--- tun2socks-core/tests/lwip_test.cpp
+++ tun2socks-core/tests/lwip_test.cpp
@@ -6,6 +6,8 @@
 #include <lwip/udp.h>
 #include <lwip/sys.h>
 #include <lwip/pbuf.h>
+#include <thread>
+#include <chrono>
 
 void init(){
     static bool initialized = false;
@@ -37,5 +39,12 @@ TEST_CASE("Test basic lwip functions"){
             pbuf_arr[i] = pbuf_alloc(pbuf_layer::PBUF_TRANSPORT, 1600, pbuf_type::PBUF_RAM);
             REQUIRE(pbuf_arr[i] != NULL);
         }
+    }
+    SECTION("Test sys_arch"){
+        using namespace std::chrono_literals;
+        auto before = sys_now();
+        std::this_thread::sleep_for(1000ms);
+        auto after = sys_now();
+        REQUIRE( (after - before < 1100 &&after - before > 900) );
     }
 }
